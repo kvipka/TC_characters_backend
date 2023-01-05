51006459c7089d4cf5794e449a05b9bd28ca62b6
kvipka90 <kvipka90@yandex.ru>
2023-01-05 19:33:28 +0300

    init springboot 3.0.1

---
+++ src/main/java/com/atwow/CharactersBackend/CharactersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atwow.CharactersBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharactersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharactersBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atwow/CharactersBackend/CharactersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atwow.CharactersBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
